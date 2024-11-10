fd9c7e95cd47d1c40886a432a5eaa54232bc0cef
ThapeloMphahlele <137503261+ThapeloMphahlele@users.noreply.github.com>
2024-11-10 18:19:27 +0200

    Added the sign up method to the Datahandler class

--- PRG272_Project/frmSignIn.cs
+++ PRG272_Project/frmSignIn.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PRG272_Project.data_layer;
+using System.IO;
 
 namespace PRG272_Project
 {
     public partial class frmSignIn : Form
     {
+        Datahandler dh = new Datahandler();
+        
         public frmSignIn()
         {
             InitializeComponent();
+        }
+
+        private void btnSignUp_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if (string.IsNullOrEmpty(username) || )
+            {
+
+            }
         }
     }
 }
