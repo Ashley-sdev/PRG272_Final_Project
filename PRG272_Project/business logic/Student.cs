e7034c8318c64f3c0259cefcfb1a557fa3a8f1e7
Ashley-sdev <muhlentuli0223@gmail.com>
2024-11-10 16:29:39 +0200

    Added solution

---
+++ PRG272_Project/business logic/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG272_Project.business_logic
+{
+    internal class Student
+    {
+    }
+}
