0b8e893b003c2a38fb6cd733394f4e1d08d24aa3
KOT22-hub <116636402+KOT22-hub@users.noreply.github.com>
2024-11-10 18:05:39 +0200

    commit

--- PRG272_Project/Form1.Designer.cs
+++ PRG272_Project/Form1.Designer.cs
@@ -30,14 +30,14 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.StudentID_textBox = new System.Windows.Forms.TextBox();
-            this.Name_textBox = new System.Windows.Forms.TextBox();
-            this.Age_textBox = new System.Windows.Forms.TextBox();
-            this.Course_textBox = new System.Windows.Forms.TextBox();
-            this.lblStudentID = new System.Windows.Forms.Label();
-            this.lblName = new System.Windows.Forms.Label();
-            this.lblAge = new System.Windows.Forms.Label();
             this.lblCourse = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblStudentID = new System.Windows.Forms.Label();
+            this.Course_textBox = new System.Windows.Forms.TextBox();
+            this.Age_textBox = new System.Windows.Forms.TextBox();
+            this.Name_textBox = new System.Windows.Forms.TextBox();
+            this.StudentID_textBox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Search_TextBox = new System.Windows.Forms.TextBox();
@@ -51,9 +51,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 68);
+            this.label1.Location = new System.Drawing.Point(9, 55);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 0;
             // 
             // groupBox1
@@ -66,84 +67,95 @@
             this.groupBox1.Controls.Add(this.Age_textBox);
             this.groupBox1.Controls.Add(this.Name_textBox);
             this.groupBox1.Controls.Add(this.StudentID_textBox);
-            this.groupBox1.Location = new System.Drawing.Point(12, 142);
+            this.groupBox1.Location = new System.Drawing.Point(9, 115);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(279, 255);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(209, 207);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Add student";
             // 
-            // StudentID_textBox
+            // lblCourse
             // 
-            this.StudentID_textBox.Location = new System.Drawing.Point(149, 30);
-            this.StudentID_textBox.Name = "StudentID_textBox";
-            this.StudentID_textBox.Size = new System.Drawing.Size(100, 22);
-            this.StudentID_textBox.TabIndex = 0;
-            // 
-            // Name_textBox
-            // 
-            this.Name_textBox.Location = new System.Drawing.Point(149, 82);
-            this.Name_textBox.Name = "Name_textBox";
-            this.Name_textBox.Size = new System.Drawing.Size(100, 22);
-            this.Name_textBox.TabIndex = 1;
-            // 
-            // Age_textBox
-            // 
-            this.Age_textBox.Location = new System.Drawing.Point(149, 132);
-            this.Age_textBox.Name = "Age_textBox";
-            this.Age_textBox.Size = new System.Drawing.Size(100, 22);
-            this.Age_textBox.TabIndex = 2;
-            // 
-            // Course_textBox
-            // 
-            this.Course_textBox.Location = new System.Drawing.Point(149, 182);
-            this.Course_textBox.Name = "Course_textBox";
-            this.Course_textBox.Size = new System.Drawing.Size(100, 22);
-            this.Course_textBox.TabIndex = 3;
-            // 
-            // lblStudentID
-            // 
-            this.lblStudentID.AutoSize = true;
-            this.lblStudentID.Location = new System.Drawing.Point(15, 35);
-            this.lblStudentID.Name = "lblStudentID";
-            this.lblStudentID.Size = new System.Drawing.Size(65, 16);
-            this.lblStudentID.TabIndex = 4;
-            this.lblStudentID.Text = "StudentID";
-            // 
-            // lblName
-            // 
-            this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(18, 82);
-            this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(44, 16);
-            this.lblName.TabIndex = 5;
-            this.lblName.Text = "Name";
+            this.lblCourse.AutoSize = true;
+            this.lblCourse.Location = new System.Drawing.Point(18, 148);
+            this.lblCourse.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCourse.Name = "lblCourse";
+            this.lblCourse.Size = new System.Drawing.Size(40, 13);
+            this.lblCourse.TabIndex = 7;
+            this.lblCourse.Text = "Course";
             // 
             // lblAge
             // 
             this.lblAge.AutoSize = true;
-            this.lblAge.Location = new System.Drawing.Point(21, 137);
+            this.lblAge.Location = new System.Drawing.Point(16, 111);
+            this.lblAge.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblAge.Name = "lblAge";
-            this.lblAge.Size = new System.Drawing.Size(32, 16);
+            this.lblAge.Size = new System.Drawing.Size(26, 13);
             this.lblAge.TabIndex = 6;
             this.lblAge.Text = "Age";
             // 
-            // lblCourse
+            // lblName
             // 
-            this.lblCourse.AutoSize = true;
-            this.lblCourse.Location = new System.Drawing.Point(24, 182);
-            this.lblCourse.Name = "lblCourse";
-            this.lblCourse.Size = new System.Drawing.Size(50, 16);
-            this.lblCourse.TabIndex = 7;
-            this.lblCourse.Text = "Course";
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(14, 67);
+            this.lblName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Name";
+            // 
+            // lblStudentID
+            // 
+            this.lblStudentID.AutoSize = true;
+            this.lblStudentID.Location = new System.Drawing.Point(11, 28);
+            this.lblStudentID.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblStudentID.Name = "lblStudentID";
+            this.lblStudentID.Size = new System.Drawing.Size(55, 13);
+            this.lblStudentID.TabIndex = 4;
+            this.lblStudentID.Text = "StudentID";
+            // 
+            // Course_textBox
+            // 
+            this.Course_textBox.Location = new System.Drawing.Point(112, 148);
+            this.Course_textBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Course_textBox.Name = "Course_textBox";
+            this.Course_textBox.Size = new System.Drawing.Size(76, 20);
+            this.Course_textBox.TabIndex = 3;
+            // 
+            // Age_textBox
+            // 
+            this.Age_textBox.Location = new System.Drawing.Point(112, 107);
+            this.Age_textBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Age_textBox.Name = "Age_textBox";
+            this.Age_textBox.Size = new System.Drawing.Size(76, 20);
+            this.Age_textBox.TabIndex = 2;
+            // 
+            // Name_textBox
+            // 
+            this.Name_textBox.Location = new System.Drawing.Point(112, 67);
+            this.Name_textBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name_textBox.Name = "Name_textBox";
+            this.Name_textBox.Size = new System.Drawing.Size(76, 20);
+            this.Name_textBox.TabIndex = 1;
+            // 
+            // StudentID_textBox
+            // 
+            this.StudentID_textBox.Location = new System.Drawing.Point(112, 24);
+            this.StudentID_textBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.StudentID_textBox.Name = "StudentID_textBox";
+            this.StudentID_textBox.Size = new System.Drawing.Size(76, 20);
+            this.StudentID_textBox.TabIndex = 0;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(258, 20);
+            this.label2.Location = new System.Drawing.Point(194, 16);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(532, 29);
+            this.label2.Size = new System.Drawing.Size(427, 24);
             this.label2.TabIndex = 2;
             this.label2.Text = "Belgium Campus Student Management Form";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -151,52 +163,58 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(310, 142);
+            this.dataGridView1.Location = new System.Drawing.Point(232, 115);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(490, 329);
+            this.dataGridView1.Size = new System.Drawing.Size(368, 267);
             this.dataGridView1.TabIndex = 3;
             // 
             // Search_TextBox
             // 
-            this.Search_TextBox.Location = new System.Drawing.Point(532, 89);
+            this.Search_TextBox.Location = new System.Drawing.Point(399, 72);
+            this.Search_TextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Search_TextBox.Name = "Search_TextBox";
-            this.Search_TextBox.Size = new System.Drawing.Size(344, 22);
+            this.Search_TextBox.Size = new System.Drawing.Size(259, 20);
             this.Search_TextBox.TabIndex = 4;
             // 
             // Search_by_studentID
             // 
-            this.Search_by_studentID.Location = new System.Drawing.Point(356, 89);
+            this.Search_by_studentID.Location = new System.Drawing.Point(267, 72);
+            this.Search_by_studentID.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Search_by_studentID.Name = "Search_by_studentID";
-            this.Search_by_studentID.Size = new System.Drawing.Size(156, 23);
+            this.Search_by_studentID.Size = new System.Drawing.Size(117, 19);
             this.Search_by_studentID.TabIndex = 5;
             this.Search_by_studentID.Text = "Search";
             this.Search_by_studentID.UseVisualStyleBackColor = true;
             // 
             // Delete_student
             // 
-            this.Delete_student.Location = new System.Drawing.Point(30, 404);
+            this.Delete_student.Location = new System.Drawing.Point(22, 328);
+            this.Delete_student.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Delete_student.Name = "Delete_student";
-            this.Delete_student.Size = new System.Drawing.Size(75, 23);
+            this.Delete_student.Size = new System.Drawing.Size(63, 19);
             this.Delete_student.TabIndex = 6;
             this.Delete_student.Text = "Delete";
             this.Delete_student.UseVisualStyleBackColor = true;
+            this.Delete_student.Click += new System.EventHandler(this.Delete_student_Click);
             // 
             // Update_Student_Details
             // 
-            this.Update_Student_Details.Location = new System.Drawing.Point(161, 404);
+            this.Update_Student_Details.Location = new System.Drawing.Point(121, 328);
+            this.Update_Student_Details.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Update_Student_Details.Name = "Update_Student_Details";
-            this.Update_Student_Details.Size = new System.Drawing.Size(75, 23);
+            this.Update_Student_Details.Size = new System.Drawing.Size(76, 19);
             this.Update_Student_Details.TabIndex = 7;
             this.Update_Student_Details.Text = "Update";
             this.Update_Student_Details.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1044, 516);
+            this.ClientSize = new System.Drawing.Size(783, 419);
             this.Controls.Add(this.Update_Student_Details);
             this.Controls.Add(this.Delete_student);
             this.Controls.Add(this.Search_by_studentID);
@@ -205,6 +223,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
