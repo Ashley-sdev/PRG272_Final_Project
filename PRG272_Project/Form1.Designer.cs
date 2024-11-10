dda899a8d1b4aef2e98bb67702ecf594527ce5f0
Ashley-sdev <muhlentuli0223@gmail.com>
2024-11-10 16:59:06 +0200

    updated the form

--- PRG272_Project/Form1.Designer.cs
+++ PRG272_Project/Form1.Designer.cs
@@ -28,13 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.StudentID_textBox = new System.Windows.Forms.TextBox();
+            this.Name_textBox = new System.Windows.Forms.TextBox();
+            this.Age_textBox = new System.Windows.Forms.TextBox();
+            this.Course_textBox = new System.Windows.Forms.TextBox();
+            this.lblStudentID = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblCourse = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblCourse);
+            this.groupBox1.Controls.Add(this.lblAge);
+            this.groupBox1.Controls.Add(this.lblName);
+            this.groupBox1.Controls.Add(this.lblStudentID);
+            this.groupBox1.Controls.Add(this.Course_textBox);
+            this.groupBox1.Controls.Add(this.Age_textBox);
+            this.groupBox1.Controls.Add(this.Name_textBox);
+            this.groupBox1.Controls.Add(this.StudentID_textBox);
+            this.groupBox1.Location = new System.Drawing.Point(15, 87);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(279, 255);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add student";
+            // 
+            // StudentID_textBox
+            // 
+            this.StudentID_textBox.Location = new System.Drawing.Point(149, 30);
+            this.StudentID_textBox.Name = "StudentID_textBox";
+            this.StudentID_textBox.Size = new System.Drawing.Size(100, 22);
+            this.StudentID_textBox.TabIndex = 0;
+            // 
+            // Name_textBox
+            // 
+            this.Name_textBox.Location = new System.Drawing.Point(149, 82);
+            this.Name_textBox.Name = "Name_textBox";
+            this.Name_textBox.Size = new System.Drawing.Size(100, 22);
+            this.Name_textBox.TabIndex = 1;
+            // 
+            // Age_textBox
+            // 
+            this.Age_textBox.Location = new System.Drawing.Point(149, 132);
+            this.Age_textBox.Name = "Age_textBox";
+            this.Age_textBox.Size = new System.Drawing.Size(100, 22);
+            this.Age_textBox.TabIndex = 2;
+            // 
+            // Course_textBox
+            // 
+            this.Course_textBox.Location = new System.Drawing.Point(149, 182);
+            this.Course_textBox.Name = "Course_textBox";
+            this.Course_textBox.Size = new System.Drawing.Size(100, 22);
+            this.Course_textBox.TabIndex = 3;
+            // 
+            // lblStudentID
+            // 
+            this.lblStudentID.AutoSize = true;
+            this.lblStudentID.Location = new System.Drawing.Point(15, 35);
+            this.lblStudentID.Name = "lblStudentID";
+            this.lblStudentID.Size = new System.Drawing.Size(65, 16);
+            this.lblStudentID.TabIndex = 4;
+            this.lblStudentID.Text = "StudentID";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(18, 82);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(44, 16);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Name";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(21, 137);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(32, 16);
+            this.lblAge.TabIndex = 6;
+            this.lblAge.Text = "Age";
+            // 
+            // lblCourse
+            // 
+            this.lblCourse.AutoSize = true;
+            this.lblCourse.Location = new System.Drawing.Point(24, 182);
+            this.lblCourse.Name = "lblCourse";
+            this.lblCourse.Size = new System.Drawing.Size(50, 16);
+            this.lblCourse.TabIndex = 7;
+            this.lblCourse.Text = "Course";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(258, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(532, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Belgium Campus Student Management Form";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(300, 96);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(490, 329);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1044, 516);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblCourse;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblStudentID;
+        private System.Windows.Forms.TextBox Course_textBox;
+        private System.Windows.Forms.TextBox Age_textBox;
+        private System.Windows.Forms.TextBox Name_textBox;
+        private System.Windows.Forms.TextBox StudentID_textBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
